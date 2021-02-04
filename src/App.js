ac1e4d07fefb39de0d35cd880417083f732bf31a
clarence-de-coder <clarence.e.a@gmail.com>
2021-02-03 22:25:26 -0500

    fcc-drum-machine

--- src/App.js
+++ src/App.js
@@ -2,43 +2,60 @@ import './App.scss';
 
 function App() {
   return (
-    <div id="drum-machine" class="d-flex flex-row justify-content-center align-items-center">
-      <div id="display">
-
-      </div>
-      <div id="inputs" class="container">
-        <div class="row no-gutters">
-          <div class="col-sm">
-            <button class="btn btn-primary drum-pad">Q</button>
+    <div id="drum-machine" class="d-flex justify-content-center align-items-center">
+      <div id="drum-machine-wrapper" class="d-flex flex-row">
+        <div id="inputs" class="container">
+          <div class="row no-gutters">
+            <div class="col-sm">
+              <button id="Tom 1" class="btn btn-primary drum-pad">
+                <audio src="/audio/tomAnalogHigh0.wav" id="Q" class="clip">
+                  Your browser does not support this audio format
+                </audio>
+                Q
+              </button>
+            </div>
+            <div class="col-sm">
+              <button id="Tom 2" class="btn btn-primary drum-pad">
+                <audio src="/audio/tomAnalogHigh0.wav" id="W" class="clip"/>W
+              </button>
+            </div>
+            <div class="col-sm">
+              <button id="Tom 3" class="btn btn-primary drum-pad">
+                <audio src="/audio/tomAnalogHigh0.wav" id="E" class="clip"/>E
+              </button>
+            </div>
           </div>
-          <div class="col-sm">
-            <button class="btn btn-primary drum-pad">W</button>
+          <div class="row no-gutters">
+            <div class="col-sm">
+              <button class="btn btn-primary drum-pad">
+                A
+              </button>
+            </div>
+            <div class="col-sm">
+              <button class="btn btn-primary drum-pad">
+                S
+              </button>           
+            </div>
+            <div class="col-sm">
+              <button class="btn btn-primary drum-pad">
+                D
+              </button>           
+            </div>
           </div>
-          <div class="col-sm">
-            <button class="btn btn-primary drum-pad">E</button>
+          <div class="row no-gutters">
+            <div class="col-sm">
+              <button class="btn btn-primary drum-pad">Z</button>
+            </div>
+            <div class="col-sm">
+              <button class="btn btn-primary drum-pad">X</button>           
+            </div>
+            <div class="col-sm">
+              <button class="btn btn-primary drum-pad">C</button>           
+            </div>
           </div>
         </div>
-        <div class="row no-gutters">
-          <div class="col-sm">
-        <button class="btn btn-primary drum-pad">A</button>
-          </div>
-          <div class="col-sm">
-        <button class="btn btn-primary drum-pad">S</button>           
-          </div>
-          <div class="col-sm">
-            <button class="btn btn-primary drum-pad">D</button>           
-          </div>
-        </div>
-        <div class="row no-gutters">
-          <div class="col-sm">
-            <button class="btn btn-primary drum-pad">Z</button>
-          </div>
-          <div class="col-sm">
-            <button class="btn btn-primary drum-pad">X</button>           
-          </div>
-          <div class="col-sm">
-            <button class="btn btn-primary drum-pad">C</button>           
-          </div>
+        <div id="display">
+          <h2>Display placeholder</h2>
         </div>
       </div>
     </div>
