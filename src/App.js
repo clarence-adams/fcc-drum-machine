a340cf1117eabe01a6d55cc04994fabd0c13b60f
clarence-de-coder <clarence@arch.localdomain>
2021-02-11 00:27:43 +0000

    drum pad

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import './App.scss';
+function Pad(props) {
+  return (
+    <button id={props.sound} class="btn btn-primary drum-pad">
+      <audio src={props.soundPath} id={props.id} class="clip"/>
+      {props.key}
+    </button>
+  )
+}
 
 function App() {
   return (
@@ -7,21 +15,18 @@ function App() {
         <div id="inputs" class="container">
           <div class="row no-gutters">
             <div class="col-sm">
-              <button id="Tom 1" class="btn btn-primary drum-pad">
-                <audio src="/audio/tomAnalogHigh0.wav" id="Q" class="clip">
-                  Your browser does not support this audio format
-                </audio>
-                Q
-              </button>
+              <Pad key="Q" id="Tom 0" soundPath="/audio/tomAnalogHigh0.wav"/>
             </div>
             <div class="col-sm">
               <button id="Tom 2" class="btn btn-primary drum-pad">
-                <audio src="/audio/tomAnalogHigh0.wav" id="W" class="clip"/>W
+                <audio src="/audio/tomAnalogHigh0.wav" id="W" class="clip"/>
+                W
               </button>
             </div>
             <div class="col-sm">
               <button id="Tom 3" class="btn btn-primary drum-pad">
-                <audio src="/audio/tomAnalogHigh0.wav" id="E" class="clip"/>E
+                <audio src="/audio/tomAnalogHigh0.wav" id="E" class="clip"/>
+                E
               </button>
             </div>
           </div>
@@ -34,12 +39,12 @@ function App() {
             <div class="col-sm">
               <button class="btn btn-primary drum-pad">
                 S
-              </button>           
+              </button>
             </div>
             <div class="col-sm">
               <button class="btn btn-primary drum-pad">
                 D
-              </button>           
+              </button>
             </div>
           </div>
           <div class="row no-gutters">
@@ -47,10 +52,10 @@ function App() {
               <button class="btn btn-primary drum-pad">Z</button>
             </div>
             <div class="col-sm">
-              <button class="btn btn-primary drum-pad">X</button>           
+              <button class="btn btn-primary drum-pad">X</button>
             </div>
             <div class="col-sm">
-              <button class="btn btn-primary drum-pad">C</button>           
+              <button class="btn btn-primary drum-pad">C</button>
             </div>
           </div>
         </div>
