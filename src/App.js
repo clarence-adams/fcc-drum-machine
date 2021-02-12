61f61312dd722b4986e365fddb4f3607bf0c173f
Clarence <clarence.e.a@gmail.com>
2021-02-12 04:01:05 +0000

    drum pad

--- src/App.js
+++ src/App.js
@@ -10,24 +10,42 @@ import snare1 from './audio/snareAnalog.wav'
 import kick0 from './audio/kickSynth.wav'
 import kick1 from './audio/kickAnalog.wav'
 
+// component that renders the drum pads
 function Pad(props) {
    useEffect(() => {
+     // variable that stores the audio file passed to the drum pad
      audio.current = document.getElementById(props.keyLabel)
   })
 
   let audio = useRef(null);
-
+  //  method that handles playing the audio file when the pad is clicked and updating the display
   const clickHandler = () => {
-    console.log(audio.current)
     if (audio.current != null) {
+      audio.current.currentTime = 0
       audio.current.play()
       props.updateDisplay(props.soundName)
     }
   }
+  // event listener that handles the same drum pad functions as clickHandler when the appropriate key is pressed as well as adding a class for css styling
+  document.addEventListener('keydown', function(event) {
+    if (audio.current != null) {
+      if (event.key.toUpperCase() === props.keyLabel) {
+        audio.current.currentTime = 0
+        audio.current.play()
+        props.updateDisplay(props.soundName)
+        document.getElementById(props.buttonId).classList.add("active")
+      }
+    }
+  })
+
+// event listener that removes the css styling added by the previous listener
+  document.addEventListener('keyup', function() {
+    document.getElementById(props.buttonId).classList.remove("active")
+  })
 
   return (
     <div class="col-sm">
-      <button id={props.buttonId} class="btn btn-primary drum-pad" onClick={clickHandler}>
+      <button id={props.buttonId} class="drum-pad" onClick={clickHandler}>
         <audio src={props.soundFile} type="audio/x-wav" id={props.keyLabel} class="clip"/>
         {props.keyLabel}
       </button>
@@ -43,24 +61,42 @@ function App() {
   }
 
   return (
-    <div id="drum-machine" class="d-flex justify-content-center align-items-center">
-      <div id="drum-machine-wrapper" class="d-flex flex-row">
+    <div id="drum-machine">
+      <div id="drum-machine-wrapper">
         <div id="inputs" class="container">
-          <div class="row no-gutters">
-            <Pad keyLabel="Q" buttonId="tom-0-button" soundName="Tom 0" soundFile={tom0} updateDisplay={updateDisplay}/>
-            <Pad keyLabel="W" buttonId="tom-1-button" soundName="Tom 1" soundFile={tom1} updateDisplay={updateDisplay}/>
-            <Pad keyLabel="E" buttonId="tom-2-button" soundName="Tom 2" soundFile={tom2} updateDisplay={updateDisplay}/>
-          </div>
-          <div class="row no-gutters">
-            <Pad keyLabel="A" buttonId="tom-3-button" soundName="Tom 3" soundFile={tom3} updateDisplay={updateDisplay}/>
-            <Pad keyLabel="S" buttonId="clap-button" soundName="Clap" soundFile={clap} updateDisplay={updateDisplay}/>
-            <Pad keyLabel="D" buttonId="snare-0-button" soundName="Snare 0" soundFile={snare0} updateDisplay={updateDisplay}/>
-          </div>
-          <div class="row no-gutters">
-            <Pad keyLabel="Z" buttonId="kick0-button" soundName="Kick 0"  soundFile={kick0} updateDisplay={updateDisplay}/>
-            <Pad keyLabel="X" buttonId="kick1-button" soundName="Kick 1"  soundFile={kick1} updateDisplay={updateDisplay}/>
-            <Pad keyLabel="C" buttonId="snare-1-button" soundName="Snare 1" soundFile={snare1} updateDisplay={updateDisplay}/>
-          </div>
+          <tr>
+            <td>
+              <Pad keyLabel="Q" buttonId="tom-0-button" soundName="Tom 0" soundFile={tom0} updateDisplay={updateDisplay}/>
+            </td>
+            <td>
+              <Pad keyLabel="W" buttonId="tom-1-button" soundName="Tom 1" soundFile={tom1} updateDisplay={updateDisplay}/>
+            </td>
+            <td>
+              <Pad keyLabel="E" buttonId="tom-2-button" soundName="Tom 2" soundFile={tom2} updateDisplay={updateDisplay}/>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <Pad keyLabel="A" buttonId="tom-3-button" soundName="Tom 3" soundFile={tom3} updateDisplay={updateDisplay}/>
+            </td>
+            <td>
+              <Pad keyLabel="S" buttonId="clap-button" soundName="Clap" soundFile={clap} updateDisplay={updateDisplay}/>
+            </td>
+            <td>
+              <Pad keyLabel="D" buttonId="snare-0-button" soundName="Snare 0" soundFile={snare0} updateDisplay={updateDisplay}/>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <Pad keyLabel="Z" buttonId="kick0-button" soundName="Kick 0"  soundFile={kick0} updateDisplay={updateDisplay}/>
+            </td>
+            <td>
+              <Pad keyLabel="X" buttonId="kick1-button" soundName="Kick 1"  soundFile={kick1} updateDisplay={updateDisplay}/>
+            </td>
+            <td>
+              <Pad keyLabel="C" buttonId="snare-1-button" soundName="Snare 1" soundFile={snare1} updateDisplay={updateDisplay}/>
+            </td>
+          </tr>
         </div>
         <div id="display">
           <h2>{lastPadPressed}</h2>
